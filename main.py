6d84ea4adae232ea17105a7d18933a000fc83e61
EndyB <endyb.dev@gmail.com>
2023-06-26 18:03:31 +1000

    Endpoint curricular grid, FastAPI

--- main.py
+++ main.py
@@ -27,13 +27,13 @@ def get_corricular_grid(
     
     skills = {type_skills[0] if len(type_skills)==1 else " y ".join(type_skills)}
 
-    prompt_b = f"""Eres un experto en formación corporativa, con más de 30 años de experiencia potenciando las habilidades de {skills} para el capital humano
+    prompt = f"""Eres un experto en formación corporativa, con más de 30 años de experiencia potenciando las habilidades de {skills} para el capital humano
                 de cientos de empresas del sector {sector_company}. Recientemente fuiste contratado por una empresa de este sector,  
                 en la cual se te asignó la responsabilidad de crear una malla curricular de cursos enfocados en {skills} para sus empleados.
                 Entonces, crea una malla curricular de cursos teniendo en cuenta las competencias de {skills} y organízala por niveles de cargo.
                 Escribe el resultado en notación JSON."""
     
-    response = ai_request(prompt_b)
+    response = ai_request(prompt)
     response = response["choices"][0]["text"]
     json_resp = json.loads(response)
     print(f"OUTPUT TYPE{type(json_resp)}")
